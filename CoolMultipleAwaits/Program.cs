0ecddf60c913f636a191774d517e12fb7854297b
pimvanroon <pimvanroon@gmail.com>
2023-11-25 17:12:15 +0100

    Initial commit

---
+++ CoolMultipleAwaits/Program.cs
@@ -0,0 +1,12 @@
+﻿using CoolMultipleAwaits;
+
+var service = new WeatherService();
+
+Console.WriteLine(TimeProvider.System.GetLocalNow());
+
+var (london, paris) = await (service.GetWeather(), service.GetWeather());
+Console.WriteLine("London:", london);
+Console.WriteLine("Paris:", paris);
+Console.WriteLine(TimeProvider.System.GetLocalNow());
+
+Console.WriteLine();
